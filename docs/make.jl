efb2ab71c7f4f20e468f1ddaedeff3c543331b98
gaelforget <gforget@mit.edu>
2022-08-31 15:11:04 -0400

    change ordering of tests

--- docs/make.jl
+++ docs/make.jl
@@ -46,9 +46,9 @@ makedocs(
 
 pth_in = joinpath(@__DIR__, "..","examples")
 pth_out = joinpath(@__DIR__, "build","examples")
-lst=("solid_body_rotation.jl","random_flow_field.jl","interactive_UI.jl",
-     "global_ocean_circulation.jl","three_dimensional_ocean.jl")
-subpth=("basics","basics","worldwide","worldwide","worldwide")
+lst=("global_ocean_circulation.jl","three_dimensional_ocean.jl",
+     "solid_body_rotation.jl","random_flow_field.jl","interactive_UI.jl")
+subpth=("worldwide","worldwide","basics","basics","worldwide")
 for ii in 1:length(lst)
     fil_in=joinpath(pth_in,subpth[ii],lst[ii])
     fil_out=joinpath(pth_out,lst[ii][1:end-2]*"html")
